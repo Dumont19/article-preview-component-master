4aee91504426d174d9e83c154ccc86958061ecc7
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2022-08-19 01:36:26 -0300

    Design desktop and funcionalities finished

--- js/index.js
+++ js/index.js
@@ -1,15 +1,54 @@
 const share = document.querySelector('.share')
-const closedShare = document.querySelector('.closed-share')
+const shareOpen = document.querySelector('.share-open')
 const openShare = document.querySelector('.open-share')
+const closedShare = document.querySelector('.closed-share')
 
-closedShare.addEventListener('mouseenter', () => {
-   if (closedShare.style.display = 'flex') {
-        closedShare.style.display = 'none'
-        openShare.style.display = 'flex'
-   }
-})
+function showShareDesktop() {
+    if (document.body.clientWidth >= 840) {
+        share.addEventListener('click', () => {
+            openShare.classList.toggle('hide')
+        })
+    } 
+}
+showShareDesktop()
 
-openShare.addEventListener('mouseleave', () => {
-        closedShare.style.display = 'flex'
-        openShare.style.display = 'none'
-})
+function showShareMobile() {
+    if (document.body.clientWidth < 840) {
+        share.addEventListener('click', () => {
+            if (openShare.style.display = 'none') {
+                openShare.style.display = 'flex'
+                closedShare.style.display = 'none'
+            }
+
+        shareOpen.addEventListener('click', () => {
+            if(openShare.style.display === 'flex') {
+                openShare.style.display = 'none'
+                closedShare.style.display = 'flex'
+            }
+        })
+        })
+    }
+}
+showShareMobile()
+
+
+
+
+// shareOpen.addEventListener('click', () => {
+//     openShare.style.display = 'none'
+//     closedShare.style.display = 'flex'
+// })
+
+
+//     if (openShare.style.display = 'none') {
+//         openShare.style.display = 'flex'
+//     }
+    
+
+// })
+
+// share.addEventListener('click', () => {
+//     openShare.style.display = 'none'
+// })
+
+
